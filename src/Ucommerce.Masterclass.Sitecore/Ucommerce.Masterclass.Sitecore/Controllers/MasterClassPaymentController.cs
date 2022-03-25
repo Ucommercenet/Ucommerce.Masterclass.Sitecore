9420a6f5584e1a4e4a59071835c991913d9a723f
Daniel Berg Frederiksen <dbf@ucommerce.dk>
2022-03-25 12:58:09 +0100

    PaymentController

--- src/Ucommerce.Masterclass.Sitecore/Ucommerce.Masterclass.Sitecore/Controllers/MasterClassPaymentController.cs
+++ src/Ucommerce.Masterclass.Sitecore/Ucommerce.Masterclass.Sitecore/Controllers/MasterClassPaymentController.cs
@@ -4,17 +4,27 @@ using Ucommerce.Api;
 using Ucommerce.Infrastructure;
 using Ucommerce.Masterclass.Models;
 
-
-namespace Ucommerce.Masterclass.Models
+namespace Ucommerce.Masterclass.Sitecore.Controllers
 {
     public class MasterClassPaymentController : Controller
     {
-        public ITransactionLibrary TransactionLibrary => ObjectFactory.Instance.Resolve<ITransactionLibrary>();
+        private static ITransactionLibrary TransactionLibrary => ObjectFactory.Instance.Resolve<ITransactionLibrary>();
 
         [System.Web.Mvc.HttpGet]
         public ActionResult Index()
         {
-            var paymentViewModel = new PaymentViewModel();
+            var availablePaymentMethods = TransactionLibrary.GetPaymentMethods(TransactionLibrary.GetShippingInformation().Country);
+            var selectedPaymentMethod = TransactionLibrary.GetBasket().Payments.FirstOrDefault()?.PaymentMethod?.PaymentMethodId ?? 0;
+
+            var paymentViewModel = new PaymentViewModel
+            {
+                AvailablePaymentMethods = availablePaymentMethods.Select(x => new SelectListItem
+                {
+                    Text = x.Name,
+                    Value = x.PaymentMethodId.ToString(),
+                    Selected = selectedPaymentMethod == x.PaymentMethodId,
+                }).ToList()
+            };
 
             return View(paymentViewModel);
         }
@@ -23,6 +33,10 @@ namespace Ucommerce.Masterclass.Models
         [HttpPost]
         public ActionResult Index(int selectedPaymentMethodId)
         {
+            TransactionLibrary.CreatePayment(selectedPaymentMethodId);
+
+            TransactionLibrary.ExecuteBasketPipeline();
+
             return Redirect("/preview");
         }
     }
