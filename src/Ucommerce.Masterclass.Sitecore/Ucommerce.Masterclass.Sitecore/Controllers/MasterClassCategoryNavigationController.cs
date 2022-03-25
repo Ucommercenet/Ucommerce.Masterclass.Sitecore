c2549286f246c18bd5c6ab70f94dd171ecc7dba1
Daniel Berg Frederiksen <dbf@ucommerce.dk>
2022-03-25 12:56:58 +0100

    CategoryNavigationController

--- src/Ucommerce.Masterclass.Sitecore/Ucommerce.Masterclass.Sitecore/Controllers/MasterClassCategoryNavigationController.cs
+++ src/Ucommerce.Masterclass.Sitecore/Ucommerce.Masterclass.Sitecore/Controllers/MasterClassCategoryNavigationController.cs
@@ -1,30 +1,45 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web.Http;
 using System.Web.Mvc;
 using Ucommerce.Api;
 using Ucommerce.Infrastructure;
 using Ucommerce.Masterclass.Models;
-using Ucommerce.Search;
 using Ucommerce.Search.Models;
 using Ucommerce.Search.Slugs;
 
-
-namespace Ucommerce.Masterclass.Models
+namespace Ucommerce.Masterclass.Sitecore.Controllers
 {
     public class MasterClassCategoryNavigationController : Controller
     {
+        private static ICatalogLibrary CatalogLibrary => ObjectFactory.Instance.Resolve<ICatalogLibrary>();
+        private static ICatalogContext CatalogContext => ObjectFactory.Instance.Resolve<ICatalogContext>();
+        private static IUrlService UrlService => ObjectFactory.Instance.Resolve<IUrlService>();
+
         public ActionResult CategoryNavigation()
         {
-            var model = new CategoryNavigationViewModel();
+            var model = new CategoryNavigationViewModel
+            {
+                Categories = MapCategories(CatalogLibrary.GetRootCategories().Results),
+                CurrentCategoryGuid = CatalogContext.CurrentCategory?.Guid ?? Guid.NewGuid()
+            };
 
             return View("/views/MasterClassCategoryNavigation/index.cshtml", model);
         }
 
-        private IList<CategoryViewModel> MapCategories(IList<Category> categories)
+        private static IList<CategoryViewModel> MapCategories(IEnumerable<Category> categories)
         {
-            return new List<CategoryViewModel>();
+            return categories.Select(category => new CategoryViewModel
+            {
+                Name = category.Name,
+                Guid = category.Guid,
+                Description = category.Description,
+                Url = UrlService.GetUrl(CatalogContext.CurrentCatalog, new []
+                {
+                    category
+                }),
+                Categories = MapCategories(CatalogLibrary.GetCategories(category.Categories).ToList())
+            }).ToList();
         }
     }
 }
